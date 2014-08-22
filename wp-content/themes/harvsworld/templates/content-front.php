3cf011fe10385758a4f07fa730e599ed948df04b
elconejito <harvey@harvsworld.com>
2014-08-22 19:39:59 -0400

    - fixed issue where the_post() wasn't firing as expected on home page due to the foreach instead of while loop. - added page headers for each section on home page - css styling

--- wp-content/themes/harvsworld/templates/content-front.php
+++ wp-content/themes/harvsworld/templates/content-front.php
@@ -1,9 +1,11 @@
 <div class="container">
     <!-- Three columns of text below the carousel -->
-    <div class="row">
+    <div class="row posts-row">
         <article class="col-md-4">
-            <h2>Coding &amp; Dev
-                <br /><small>more in <a href="<?php echo esc_url( get_category_link( get_cat_ID('coding') ) ); ?>" title="Coding Category">Coding</a></small></h2>
+            <div class="page-header">
+                <h2>Coding &amp; Dev
+                    <br /><small>more in <a href="<?php echo esc_url( get_category_link( get_cat_ID('coding') ) ); ?>" title="Coding Category">Coding</a></small></h2>
+            </div>
             <?php if ( $frontPagePosts['coding']->have_posts() ) : $frontPagePosts['coding']->the_post(); ?>
                 <header class="entry-header">
                     <h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
@@ -20,8 +22,10 @@
             <?php endif; ?>
         </article>
         <article class="col-md-4">
-            <h2>Quick Hits
-                <br /><small>more in <a href="<?php echo esc_url( get_category_link( get_cat_ID('quick-hits') ) ); ?>" title="Quick HitsCategory">Quick Hits</a></small></h2>
+            <div class="page-header">
+                <h2>Quick Hits
+                    <br /><small>more in <a href="<?php echo esc_url( get_category_link( get_cat_ID('quick-hits') ) ); ?>" title="Quick HitsCategory">Quick Hits</a></small></h2>
+            </div>
             <?php if ( $frontPagePosts['quick']->have_posts() ) : $frontPagePosts['quick']->the_post(); ?>
                 <header class="entry-header">
                     <h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
@@ -39,22 +43,24 @@
         </article>
         <div class="col-md-4">
             <!-- Twitter -->
-            <h2>Tweets</h2>
-            <a class="twitter-timeline" href="https://twitter.com/harveyramos" data-widget-id="331604685595222016" data-chrome="nofooter">Tweets by @harveyramos</a>
-            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+            <div class="page-header">
+                <h2>Tweets</h2>
+            </div>
+            <!--<a class="twitter-timeline" href="https://twitter.com/harveyramos" data-widget-id="265274759128940544" data-chrome="nofooter">Tweets by @harveyramos</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>-->
         </div>
     </div><!-- /.row -->
     <div class="row">
         <div class="col-md-12">
-            <h2>Latest Posts
-                <br /><small>more in <a href="<?php echo esc_url( get_permalink( get_page_by_title( 'Blog' ) ) ); ?>" title="the blog">the Blog</a></small></h2>
+            <div class="page-header">
+                <h2>Latest Posts
+                    <br /><small>more in <a href="<?php echo esc_url( get_permalink( get_page_by_title( 'Blog' ) ) ); ?>" title="the blog">the Blog</a></small></h2>
+            </div>
             <?php
-            $latestPosts = $frontPagePosts['latest']->get_posts();
-
-            if ( $latestPosts ) :
-                foreach ( array_chunk($latestPosts, 2) as $row ) : ?>
-            <div class="row">
-                <?php foreach ( $row as $post ) : ?>
+            if ( $frontPagePosts['latest']->have_posts() ) :
+                foreach ( array_chunk($frontPagePosts['latest']->get_posts(), 2) as $row ) : ?>
+            <div class="row posts-row">
+                <?php foreach ( $row as $post ) : setup_postdata( $post ); ?>
                     <article class="col-md-6">
                         <header class="entry-header">
                             <h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
