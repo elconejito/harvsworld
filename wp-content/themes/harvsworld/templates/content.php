4cdc69efda15e3b6d8f54504f44437b3dacd05bd
Harvey Ramos <harvey@harvsworld.com>
2014-08-25 15:58:10 +0000

    fixed post has_post_thumbnail() not working properly, per http://codex.wordpress.org/Function_Reference/has_post_thumbnail to use get_the_post_thumbnail() != '' instead. fixed column layout on blog page.

--- wp-content/themes/harvsworld/templates/content.php
+++ wp-content/themes/harvsworld/templates/content.php
@@ -3,7 +3,15 @@
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
-  <div class="entry-summary">
-    <?php the_excerpt(); ?>
+  <div class="row">
+    <?php if ( '' != get_the_post_thumbnail() ) : $summaryColClass = 'col-md-9'; ?>
+    <div class="col-md-3">
+      <?php the_post_thumbnail( 'thumbnail', [ 'class' => 'img-responsive'] ); ?>
+    </div>
+  
+    <?php else : $summaryColClass = 'col-md-12'; endif; ?>
+    <div class="entry-summary <?php echo $summaryColClass; ?>">
+      <?php the_excerpt(); ?>
+    </div>
   </div>
 </article>
