e38332bbe9f844bcb347876afe4b523ca681443e
Harvey Ramos <harvey@harvsworld.com>
2014-08-25 23:59:47 +0000

    updated footer widgets for 3 columns, added powered by verbage and copyright to footer

--- wp-content/themes/harvsworld/lib/init.php
+++ wp-content/themes/harvsworld/lib/init.php
@@ -46,8 +46,24 @@ function roots_widgets_init() {
   ));
 
   register_sidebar(array(
-    'name'          => __('Footer', 'roots'),
-    'id'            => 'sidebar-footer',
+    'name'          => __('Footer 1', 'roots'),
+    'id'            => 'sidebar-footer-1',
+    'before_widget' => '<section class="widget %1$s %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>',
+  ));
+  register_sidebar(array(
+    'name'          => __('Footer 2', 'roots'),
+    'id'            => 'sidebar-footer-2',
+    'before_widget' => '<section class="widget %1$s %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>',
+  ));
+  register_sidebar(array(
+    'name'          => __('Footer 3', 'roots'),
+    'id'            => 'sidebar-footer-3',
     'before_widget' => '<section class="widget %1$s %2$s">',
     'after_widget'  => '</section>',
     'before_title'  => '<h3>',
--- wp-content/themes/harvsworld/templates/footer.php
+++ wp-content/themes/harvsworld/templates/footer.php
@@ -1,6 +1,19 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+    <div class="row">
+      <div class="col-md-4">
+        <?php dynamic_sidebar('sidebar-footer-1'); ?>
+      </div>
+      <div class="col-md-4">
+        <?php dynamic_sidebar('sidebar-footer-2'); ?>
+      </div>
+      <div class="col-md-4">
+        <?php dynamic_sidebar('sidebar-footer-3'); ?>
+      </div>
+    </div>
+    <div class="row">
+      <p>Copyright &copy; <?php echo date("Y"); ?> harvsworld.com. Powered by Little Rabbit Studios.</p>
+    </div>
   </div>
 </footer>
 
