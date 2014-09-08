dd901c93c1690dfa80c691db5bdba6f149abf9c7
elconejito <harvey@harvsworld.com>
2014-09-07 20:29:38 -0400

    moved up to parent page link to top of page. added down to sub galleries link next to it. added heading to sub galleries

--- wp-content/themes/harvsworld/template-photos.php
+++ wp-content/themes/harvsworld/template-photos.php
@@ -5,8 +5,13 @@ Template Name: Photo Gallery Template
 ?>
 
 <?php while (have_posts()) : the_post(); ?>
-    <?php get_template_part('templates/page', 'header'); ?>
     <?php $page_children = get_pages( ['parent' => $post->ID] ); ?>
-    <?php $page_parent = get_pages( ['parent' => $post->ID, 'hierarchical'=>0] ); ?>
+    <?php if ( $post->post_parent != 0 ) : $parent = get_post( $post->post_parent ); ?>
+        <a href="<?php echo get_permalink($parent->ID); ?>" class="pull-left btn btn-default"><span class="glyphicon glyphicon-chevron-up"></span> up to <?php echo $parent->post_title; ?></a>
+    <?php endif; ?>
+    <?php if ( is_array($page_children) && count($page_children) > 0 ) : ?>
+        <a href="#subgalleries" class="pull-left btn btn-default"><span class="glyphicon glyphicon-chevron-down"></span> more galleries</a>
+    <?php endif; ?>
+    <?php get_template_part('templates/page', 'header'); ?>
     <?php include(locate_template('templates/content-single-gallery.php')); ?>
 <?php endwhile; ?>
--- wp-content/themes/harvsworld/templates/content-single-gallery.php
+++ wp-content/themes/harvsworld/templates/content-single-gallery.php
@@ -1,14 +1,10 @@
 <article <?php post_class(); ?>>
     <div class="entry-content">
-        <?php if ( $post->post_parent != 0 ) : $parent = get_post( $post->post_parent ); ?>
-            <a href="<?php echo get_permalink($parent->ID); ?>" class="btn btn-default"><span class="glyphicon glyphicon-chevron-up"></span> up to <?php echo $parent->post_title; ?></a>
-        <?php endif; ?>
         <?php the_content(); ?>
-        <?php // echo hw_slick_gallery(); ?>
-
-        <?php
-        if ( is_array($page_children) && count($page_children) > 0 ) :
-            foreach ( array_chunk($page_children, 3) as $row ) : ?>
+        <?php if ( is_array($page_children) && count($page_children) > 0 ) : ?>
+            <hr />
+            <h2><a name="subgalleries"></a>Sub Galleries</h2>
+            <?php foreach ( array_chunk($page_children, 3) as $row ) : ?>
                 <div class="row posts-row">
                     <?php foreach ( $row as $post ) : setup_postdata( $post ); ?>
                         <article class="col-md-4 hentry">
@@ -21,12 +17,10 @@
                             </div>
                         </article>
                     <?php endforeach; ?>
-                </div>
-
-            <?php endforeach;
-        endif;
-        ?>
-    </div>
+                </div><!-- /.row.posts-row -->
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </div><!-- /.entry-content -->
     <footer class="entry-footer">
         <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
     </footer>
