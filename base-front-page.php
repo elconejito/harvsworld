ea310a5ee1e14f44cefbdbb45eb62f013016534e
Harvey Ramos <harvey@harvsworld.com>
2014-11-21 03:49:28 +0000

    more layout tweaks to home page. css & structure

--- base-front-page.php
+++ base-front-page.php
@@ -5,13 +5,12 @@
     do_action('get_header');
     get_template_part('templates/header');
   ?>
-  <div id="home" class="para-section" <?php echo hw_home_featured_background(); ?>>
-    <div class="container">
-      <h1>home</h1>
-    </div>
-  </div>
+  
+  <?php get_template_part('templates/content-front-home'); ?>
   
   <?php get_template_part('templates/content-front-profile'); ?>
+  
+  <?php get_template_part('templates/content-front-social'); ?>
   
   <div id="social" class="para-section">
     <div class="container">
