eea397d0b187a90d5aab2b00ce6ffde5c5b5b3ba
Harvey Ramos <harvey@harvsworld.com>
2015-03-08 03:39:04 +0000

    Added older/newer posts link

--- index.php
+++ index.php
@@ -18,4 +18,9 @@
 	?>
 <?php endwhile; ?>
 
-<?php the_posts_navigation(); ?>
+<nav class="post-nav">
+	<ul class="pager">
+		<li class="previous"><?php next_posts_link('&laquo; Older Posts'); ?></li>
+		<li class="next"><?php previous_posts_link('Newer Posts &raquo;'); ?></li>
+	</ul>
+</nav>
--- templates/footer.php
+++ templates/footer.php
@@ -11,6 +11,9 @@
 				<?php dynamic_sidebar( 'sidebar-footer-right' ); ?>
 			</div>
 		</div>
+		<div class="row">
+			&copy; 2015 harvsworld.com. Built and powered by <a href="http://littlerabbitstudios.com" target="_blank">Little Rabbit Studios</a>
+		</div>
 	</div>
 	<?php wp_footer(); ?>
 </footer>
