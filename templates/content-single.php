394f5e299c0aaeb19b18f32869e49416124b62c6
Harvey Ramos <harvey@harvsworld.com>
2015-03-08 03:46:21 +0000

    change single post next/prev arrows

--- templates/content-single.php
+++ templates/content-single.php
@@ -14,8 +14,8 @@
 		</footer>
 		<nav class="post-nav">
 			<ul class="pager">
-				<li class="previous"><?php previous_post_link( '%link', '&larr; %title' ); ?></li>
-				<li class="next"><?php next_post_link( '%link', '%title &rarr;' ); ?></li>
+				<li class="previous"><?php previous_post_link( '%link', '&laquo; %title' ); ?></li>
+				<li class="next"><?php next_post_link( '%link', '%title &raquo;' ); ?></li>
 			</ul>
 		</nav>
 		<?php comments_template( '/templates/comments.php' ); ?>
