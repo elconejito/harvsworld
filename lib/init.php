8cd3b2ab5eca4dce05fff0ccc1703df0da7d7a35
Harvey Ramos <harvey@littlerabbitstudios.com>
2016-11-16 13:51:31 +0000

    Version bump, moving deck chairs

--- lib/init.php
+++ lib/init.php
@@ -23,12 +23,6 @@ function setup() {
 		'primary_navigation' => __( 'Primary Navigation', 'sage' )
 	] );
 
-	// Add post thumbnails
-	// http://codex.wordpress.org/Post_Thumbnails
-	// http://codex.wordpress.org/Function_Reference/set_post_thumbnail_size
-	// http://codex.wordpress.org/Function_Reference/add_image_size
-	add_theme_support( 'post-thumbnails' );
-
 	// Add post formats
 	// http://codex.wordpress.org/Post_Formats
 	add_theme_support( 'post-formats', [ 'aside', 'gallery', 'link', 'image', 'quote', 'video', 'audio' ] );
@@ -38,7 +32,13 @@ function setup() {
 	add_theme_support( 'html5', [ 'caption', 'comment-form', 'comment-list' ] );
 
 	// Tell the TinyMCE editor to use a custom stylesheet
-	add_editor_style( Assets\asset_path( 'styles/editor-style.css' ) );
+	// add_editor_style( Assets\asset_path( 'styles/editor-style.css' ) );
+
+	// Add post thumbnails
+	// http://codex.wordpress.org/Post_Thumbnails
+	// http://codex.wordpress.org/Function_Reference/set_post_thumbnail_size
+	// http://codex.wordpress.org/Function_Reference/add_image_size
+	add_theme_support( 'post-thumbnails' );
 	
 	/**
      * Add Custom Image Sizes
